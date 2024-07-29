5d7017a51bceaee41ae272b00cdcfe54cd463db4
Sarina Li <74844953+sarinali@users.noreply.github.com>
2024-07-29 10:11:23 -0400

    move into logs

--- graph_overlay.py
+++ graph_overlay.py
@@ -11,7 +11,7 @@ class SphereSimulation:
 simulations = []
 num_values = 0
 
-with open('loss_logs.txt', 'r') as file:
+with open('logs/loss_logs.txt', 'r') as file:
     # Read the lines
     lines = file.readlines()
 
--- run_simulation.sh
+++ run_simulation.sh
@@ -3,5 +3,5 @@ num_steps=50
 
 for i in {1..10}
 do
-  /Users/sarinali/anaconda3/bin/python /Users/sarinali/Projects/VectorAdam/demo1.py $i $num_steps >> output_readable.txt 2>> loss_logs.txt
+  /Users/sarinali/anaconda3/bin/python /Users/sarinali/Projects/VectorAdam/demo1.py $i $num_steps >> logs/output_readable.txt 2>> logs/loss_logs.txt
 done
